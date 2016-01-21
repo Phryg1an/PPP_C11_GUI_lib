15749d49890a5af4077c8e3d375eabb900a5dac7
Phryg1an <ellie.glitch@gmail.com>
2016-01-21 17:54:08 +0000

    Updated to 12.7.8 in PPP

--- try_this.cpp
+++ try_this.cpp
@@ -33,6 +33,9 @@ using namespace Graph_lib;
 int main()
 try
 {
+
+	//---------------------set window
+
 	Point tl {100,100}; //top left corner of our window
 
 	Simple_window win {tl,600,400,"Canvas"};
@@ -41,6 +44,8 @@ try
 	//title: Canvas
 
 	win.wait_for_button(); // display!
+
+	//---------------------set x axis
 
 	Axis xa {Axis::x, Point{20,300}, 280, 10, "x axis"};
 	//an axis is a kind of shape
@@ -54,12 +59,90 @@ try
 	win.set_label("Canvas #2"); //relabel the window
 	win.wait_for_button(); //display!
 
+	//---------------------set y axis
+
 	Axis ya {Axis::y, Point{20,300}, 280, 10, "y axis"};
 	ya.set_color(Color::cyan); //choose a color
 	ya.label.set_color(Color::dark_red); //choose a color for the text
 	win.attach(ya);
 	win.set_label("Canvas #3");
 	win.wait_for_button(); //display!
+
+	//---------------------plot sine wave
+
+	Function sine {sin,0,100,Point{20,150},1000,50,50}; //sine curve
+		//plot sin() in the range {0:100} with {0,0} at {20,150}
+		//using 1000 points; scale x values *50, scale y values *50
+
+	win.attach(sine);
+	win.set_label("Canvas #4");
+	win.wait_for_button();
+
+	//---------------------change sine colour and plot triangle
+
+	sine.set_color(Color::blue); //we changed our mind about sine's color
+
+	Polygon poly; //a polygon
+	poly.add(Point{300,200}); //three points make a triangle
+	poly.add(Point{350,100});
+	poly.add(Point{400,200});
+
+	poly.set_color(Color::red);
+	poly.set_style(Line_style::dash);
+	win.attach(poly);
+
+	win.set_label("Canvas #5");
+	win.wait_for_button();
+
+	//---------------------make a rectangle
+
+	Rectangle r (Point{200,200}, 100, 50);
+	win.attach(r);
+	win.set_label("Canvas #6");
+	win.wait_for_button();
+
+	//---------------------make something that looks like a rectangle,
+	//---------------------but isn't.........
+
+	Closed_polyline poly_rect;
+	poly_rect.add(Point{100,50});
+	poly_rect.add(Point{200,50});
+	poly_rect.add(Point{200,100});
+	poly_rect.add(Point{100,100});
+	win.attach(poly_rect);
+
+	win.wait_for_button();
+
+	poly_rect.add(Point{50,75});
+
+	win.wait_for_button();
+
+	//---------------------fill in the shapes!
+
+	r.set_fill_color(Color::yellow);
+	poly.set_style(Line_style(Line_style::dash,4));
+	poly_rect.set_style(Line_style(Line_style::dash,2));
+	poly_rect.set_fill_color(Color::green);
+	win.set_label("Canvas #7");
+	win.wait_for_button();
+
+	//---------------------add some text
+
+	Text t {Point{150,150}, "Hello, graphical world!"};
+	win.attach(t);
+	win.set_label("Canvas #8");
+	win.wait_for_button();
+
+	//---------------------format the text
+
+	t.set_font(Graph_lib::Font::times_bold);
+	t.set_font_size(20);
+	win.set_label("Canvas #9");
+	win.wait_for_button();
+
+
+
+
 
 }
 catch(std::exception& e) {
