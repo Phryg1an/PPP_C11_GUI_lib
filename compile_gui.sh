3c589c62cbca76871827d3e505d135cc349eeb74
Phryg1an <ellie.glitch@gmail.com>
2016-01-25 17:43:07 +0000

    Tidied up directory structure

--- compile_gui.sh
+++ compile_gui.sh
@@ -10,7 +10,7 @@
 
 if [ $1 ]
 then
-	g++ -w -Wall -std=c++11 Graph.cpp Window.cpp GUI.cpp Simple_window.cpp $1.cpp `fltk-config --ldflags --use-images` -o $1 2>&1 | tee log.txt
+	g++ -w -Wall -std=c++11 BS_GUI/Graph.cpp BS_GUI/Window.cpp BS_GUI/GUI.cpp BS_GUI/Simple_window.cpp $1.cpp `fltk-config --ldflags --use-images` -o $1 2>&1 | tee log.txt
 else
 	echo "ERROR: usage - ./compile_gui.sh <C++11 filename without extension>"
 fi
