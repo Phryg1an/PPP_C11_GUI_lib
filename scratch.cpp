db0cdd29810a436b24476ec6a8457e074fd96f25
Phryg1an <ellie.glitch@gmail.com>
2016-08-01 23:24:14 +0100

    changed Fl reference to FL in fltk.h

--- scratch.cpp
+++ scratch.cpp
@@ -23,28 +23,28 @@ try
 	Point cen{500,500};
 
 	SoftBox sb1(cen,300,200,20);
-	//Rectangle r1(cen,400,300);
+	Rectangle r1(cen,400,300);
 
-	// Arrow a1(r1.center(), ne(r1),20,20);
-	// Arrow a2(r1.center(), nw(r1),20,20);
-	// Arrow a3(r1.center(), se(r1),20,20);
-	// Arrow a4(r1.center(), sw(r1),20,20);
-	// Arrow a5(r1.center(), nn(r1),20,20);
-	// Arrow a6(r1.center(), ss(r1),20,20);
-	// Arrow a7(r1.center(), ww(r1),20,20);
-	// Arrow a8(r1.center(), ee(r1),20,20);
+	Arrow a1(r1.center(), ne(r1),20,20);
+	Arrow a2(r1.center(), nw(r1),20,20);
+	Arrow a3(r1.center(), se(r1),20,20);
+	Arrow a4(r1.center(), sw(r1),20,20);
+	Arrow a5(r1.center(), nn(r1),20,20);
+	Arrow a6(r1.center(), ss(r1),20,20);
+	Arrow a7(r1.center(), ww(r1),20,20);
+	Arrow a8(r1.center(), ee(r1),20,20);
 
-	// win.attach(a1);
-	// win.attach(a2);
-	// win.attach(a3);
-	// win.attach(a4);
-	// win.attach(a5);
-	// win.attach(a6);
-	// win.attach(a7);
-	// win.attach(a8);
+	win.attach(a1);
+	win.attach(a2);
+	win.attach(a3);
+	win.attach(a4);
+	win.attach(a5);
+	win.attach(a6);
+	win.attach(a7);
+	win.attach(a8);
 
 
-	//win.attach(r1);
+	win.attach(r1);
 	win.attach(sb1);
 
 	win.wait_for_button();
