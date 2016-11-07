ea014ffafe364b49dbaeeccf982a02f2e7afc49a
Phryg1an <ellie.glitch@gmail.com>
2016-11-07 18:27:32 +0000

    Regular Polygon header code cleaned up

--- BS_GUI/Regular_polygon.h
+++ BS_GUI/Regular_polygon.h
@@ -25,23 +25,10 @@ struct Regular_polygon : Shape {
 	}
 
 	void draw_lines() const;
-
-//	void set_fill_color(Color col) { fcolor = col; }
-//	Color fill_color() { return fcolor; }
-
 	int width() const { return w; }
 	int sides() const { return s; }
 	Point center() const { return {point(0).x, point(0).y}; }
-	// int y_offset() const { return y_off; }
 
-	// Point ne() const { return {point(0).x + (w/2), point(0).y - y_off}; }
-	// Point nw() const { return {point(0).x - (w/2), point(0).y - y_off}; }
-	// Point se() const { return {point(0).x + (w/2), point(0).y + y_off}; }
-	// Point sw() const { return {point(0).x - (w/2), point(0).y + y_off}; }	
-	// Point nn() const { return {point(0).x, point(0).y - y_off}; }
-	// Point ee() const { return {point(0).x + w, point(0).y}; }
-	// Point ss() const { return {point(0).x, point(0).y + y_off}; }
-	// Point ww() const { return {point(0).x - w, point(0).y}; }
 
 private:
 	
@@ -49,14 +36,8 @@ private:
 	int s;		// number of sides
 	int a;		// initial angle from center in degrees
 	double ar = a * (PI / 180.0); //initial angle from center in radians
-	// int y_off = (int)((double)w * (cos((30.0) * (PI / 180.00)))); // y offset
-	// int int_ang_tot = (s - 2) * 180;
-	// double int_ang = (double)int_ang_tot / (double)s;
 	Point p;
 
-	
-
-//	Color fcolor;	// fill color; 0 means "no fill"
 };
 
 
